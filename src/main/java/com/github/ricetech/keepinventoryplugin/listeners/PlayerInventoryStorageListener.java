22e18c6f8d53a4e5d41fd6c55585215eaf95b52c
Eric C <39015447+ricetech@users.noreply.github.com>
2023-05-15 04:11:18 -0400

    Fix repeated items

--- src/main/java/com/github/ricetech/keepinventoryplugin/listeners/PlayerInventoryStorageListener.java
+++ src/main/java/com/github/ricetech/keepinventoryplugin/listeners/PlayerInventoryStorageListener.java
@@ -1,6 +1,7 @@
 package com.github.ricetech.keepinventoryplugin.listeners;
 
 import com.github.ricetech.keepinventoryplugin.commands.RestoreInventoryCommand;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,9 +16,12 @@ public class PlayerInventoryStorageListener implements Listener {
         PlayerInventory inventory = p.getInventory();
         int numInvItems = inventory.getContents().length;
         int numArmor = inventory.getArmorContents().length;
-        ItemStack[] items = new ItemStack[numInvItems + numArmor];
+        ItemStack[] items = new ItemStack[45];
         System.arraycopy(inventory.getContents(), 0, items, 0, numInvItems);
         System.arraycopy(inventory.getArmorContents(), 0, items, numInvItems, numArmor);
+        for (int i = numInvItems + numArmor; i < 45; i++) {
+            items[i] = new ItemStack(Material.AIR);
+        }
 
         RestoreInventoryCommand.putInventory(p.getName(), items);
     }
